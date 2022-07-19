972c1aadd174d86f4ba1dbd2849a37848e765e4f
kosukecoulomb <jobch.coulomb@gmail.com>
2022-07-20 02:08:52 +0900

    [update]connect_mysql

--- src/book_log.php
+++ src/book_log.php
@@ -1,38 +1,111 @@
 <?php
 
-function createLog()
+function validate($log) {
+    $errors = [];
+
+    if (!strlen($log['title'])) {
+        $errors['title'] = '書籍名を入力してください';
+    } elseif (strlen($log['title']) > 255) {
+        $errors['title'] = '書籍名は255文字以内で入力してください';
+    }
+
+    if (!strlen($log['author'])) {
+        $errors['author'] = '著者名を入力してください';
+    } elseif (strlen($log['author']) > 255) {
+        $errors['author'] = '書籍名は255文字以内で入力してください';
+    }
+
+    if (!in_array($log['status'], [ '未読','読んでいる','読了'], true)) {
+        $errors['status'] = '未読,読んでる,読了のいづれかを入力してください';
+    }
+
+    if ($log['score'] < 1 || $log['score'] > 5) {
+        $errors['logs'] = '評価は半角1〜5で入れてください';
+    }
+
+    if (!strlen($log['impression'])) {
+        $errors['impression'] = '感想を入力してください';
+    } elseif (strlen($log['impression']) > 255) {
+        $errors['impression'] = '感想は255文字以内で入力してください';
+    }
+
+    return $errors;
+}
+
+$link = mysqli_connect('db', 'book_log', 'pass', 'book_log');
+
+function dbConnect()
 {
+    $link = mysqli_connect('db', 'book_log', 'pass', 'book_log');
+
+    if (!$link) {
+        echo 'Error: データベースに接続できませんでした' . PHP_EOL;
+        echo 'Debugging error:' . mysqli_connect_error() . PHP_EOL;
+        exit;
+    }
+
+    return $link;
+}
+
+function createLog($link)
+{
+    $log = [];
     echo '読書ログを登録してください' . PHP_EOL;
 
     echo '書籍名：';
-    $title = trim(fgets(STDIN));
+    $log['title'] = trim(fgets(STDIN));
 
     echo '著者名：';
-    $author = trim(fgets(STDIN));
+    $log['author'] = trim(fgets(STDIN));
 
-    echo '読書状況(未読,読んでる,読了)：';
-    $status = trim(fgets(STDIN));
+    echo '読書状況(未読,読んでる,読了)： ';
+    $log['status'] = trim(fgets(STDIN));
 
     echo '評価(5点満点の整数)：';
-    $score = trim(fgets(STDIN));
+    $log['score'] = (int)trim(fgets(STDIN));
 
-    echo '感想：';
-    $impression = trim(fgets(STDIN));
+    echo '感想： ';
+    $log['impression'] = trim(fgets(STDIN));
 
-    echo '登録が完了しました' . PHP_EOL . PHP_EOL;
+    $validated = validate($log);
+    if (count($validated) > 0) {
+        foreach ($validated as $error) {
+            echo $error . PHP_EOL;
+        }
+        return;
+    }
 
-    return[
-        'title' => $title,
-        'author' => $author,
-        'status' => $status,
-        'score' => $score,
-        'impression' => $impression,
-    ];
+    $sql = <<<EOT
+INSERT INTO booklog (
+    title,
+    author,
+    status,
+    score,
+    impression
+) VALUE (
+    "{$log['title']}",
+    "{$log['author']}",
+    "{$log['status']}",
+    "{$log['score']}",
+    "{$log['impression']}"
+)
+EOT;
+
+    $result = mysqli_query($link, $sql);
+    if ($result) {
+        echo 'データを追加しました' . PHP_EOL;
+    } else {
+        echo 'ERROR: データの追加に失敗しました' . PHP_EOL;
+        echo 'Debugging error:' . mysqli_error($link) . PHP_EOL . PHP_EOL;
+    }
 }
 
-function listLogs($logs)
+function listLogs($link)
 {
-    foreach ($logs as $log) {
+    $sql = 'SELECT id, title, author, status, score, impression FROM booklog';
+    $results = mysqli_query($link, $sql);
+
+    while ($log = mysqli_fetch_assoc($results)) {
         echo '読書ログを表示します' . PHP_EOL;
         echo '書籍名：' . $log['title'] . PHP_EOL;
         echo '著者名：' . $log['author'] . PHP_EOL;
@@ -40,10 +113,13 @@ function listLogs($logs)
         echo '評価：' . $log['score'] . PHP_EOL;
         echo '感想：' . $log['impression'] . PHP_EOL;
         echo '------------------------' . PHP_EOL;
-    };
+    }
+
+    mysqli_free_result($results);
 }
 
-$logs = [];
+
+
 
 while (true) {
     echo '1. 読書ログを登録' . PHP_EOL;
@@ -53,10 +129,12 @@ while (true) {
     $num = trim(fgets(STDIN));
 
     if ($num === '1') {
-        $logs[] = createLog();
+        createLog($link);
     } elseif ($num === '2') {
-        listLogs($logs);
+        listLogs($link);
     } elseif ($num === '9') {
+        mysqli_close($link);
+        echo 'データベースとの接続を切断しました' . PHP_EOL;
         break;
     }
 }
--- src/memo.php
+++ src/memo.php
@@ -1,28 +1,78 @@
 <?php
 
-function createMemo()
-{
-    echo 'タイトル:';
-    $title = trim(fgets(STDIN));
-    echo '内容:';
-    $body = trim(fgets(STDIN));
-    echo '登録が完了しました' . PHP_EOL . PHP_EOL;
-    return [
-        'title' => $title,
-        'body' => $body,
-    ];
+function validate($memo) {
+    $errors = [];
+
+    if (!strlen($memo['title'])) {
+        $errors['title'] = 'タイトルを入力してください';
+    }
+
+    if (!strlen($memo['body'])) {
+        $errors['body'] = '内容を入力してください';
+    }
 }
 
-function listMemos($memos)
+$link = mysqli_connect('db', 'book_log', 'pass', 'book_log');
+
+function dbConnect() {
+    $link = mysqli_connect('db', 'book_log', 'pass', 'book_log');
+
+    if (!$link) {
+        echo 'Error: データベースに接続できませんでした' . PHP_EOL;
+        echo 'Debugging error:' . mysqli_connect_error() . PHP_EOL;
+        exit;
+    }
+}
+
+function createMemo($link)
 {
-    foreach ($memos as $memo) {
+    $memo = [];
+
+    echo 'タイトル:';
+    $memo['title'] = trim(fgets(STDIN));
+    echo '内容:';
+    $memo['body'] = trim(fgets(STDIN));
+
+    $validated = validate($memo);
+    if (count($validated) > 0) {
+        foreach ($validated as $error) {
+            echo $error . PHP_EOL;
+        }
+        return;
+    }
+
+    $sql = <<<EOT
+INSERT INTO memo (
+    title,
+    body
+) VALUE (
+    "{$memo['title']}",
+    "{$memo['body']}"
+)
+EOT;
+
+    $result = mysqli_query($link, $sql);
+    if ($result) {
+        echo 'メモを登録しました' . PHP_EOL;
+    } else {
+        echo 'メモの追加に失敗しました' . PHP_EOL;
+        echo 'Debugging error:' . mysqli_error($link) . PHP_EOL . PHP_EOL;
+    }
+}
+
+function listMemos($link)
+{
+$sql = 'SELECT id, title, body FROM memo';
+$results = mysqli_query($link, $sql);
+
+    while ($memo = mysqli_fetch_assoc($results)) {
         echo $memo['title'] . PHP_EOL;
         echo $memo['body'] . PHP_EOL;
         echo '-------' . PHP_EOL;
-    };
-}
+    }
 
-$memos = [];
+    mysqli_free_result($results);
+}
 
 while (true) {
     echo '1,メモを取る' . PHP_EOL;
@@ -32,9 +82,9 @@ while (true) {
     $num = trim(fgets(STDIN));
 
     if ($num === '1') {
-        $memos[] = createMemo();
+        createMemo($link);
     } elseif ($num === '2') {
-        listMemos($memos);
+        listMemos($link);
     } elseif ($num === '9') {
         echo 'アプリを終了' . PHP_EOL;
         break;
