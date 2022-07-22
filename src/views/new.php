1a43c3558c68b374c489af6c2d989d9926992027
kosukecoulomb <jobch.coulomb@gmail.com>
2022-07-22 20:05:53 +0900

    [update]validation

--- src/views/new.php
+++ src/views/new.php
@@ -21,36 +21,36 @@
 
         <div>
             <label for="title">書籍名</label>
-            <input type="text" id="title" name="title">
+            <input type="text" id="title" name="title" value="<?php echo $review['title'] ?>">
         </div>
         <div>
             <label for="author">著者名</label>
-            <input type="text" id="author" name="author">
+            <input type="text" id="author" name="author" value="<?php echo $review['author'] ?>">
         </div>
         <div>
             <label>読書状況</label>
             <div>
                 <div>
-                    <input type="radio" name="status" id="status1" value="未読">
+                    <input type="radio" name="status" id="status1" value="未読" <?php echo ($review['status'] === '未読') ? 'checked' : ''; ?>>
                     <label for="status1">未読</label>
                 </div>
                 <div>
-                    <input type="radio" name="status" id="status2" value="読書中">
+                    <input type="radio" name="status" id="status2" value="読書中" <?php echo ($review['status'] === '読書中') ? 'checked' : ''; ?>>
                     <label for="status2">読書中</label>
                 </div>
                 <div>
-                    <input class="form-check-input" type="radio" name="status" id="status3" value="読了">
+                    <input class="form-check-input" type="radio" name="status" id="status3" value="読了" <?php echo ($review['status'] === '読了') ? 'checked' : ''; ?>>
                     <label for="status3">読了</label>
                 </div>
             </div>
         </div>
         <div>
             <label for="score">評価(5点満点の整数)</label>
-            <input type="number" id="score" name="score">
+            <input type="number" id="score" name="score" value="<?php echo $review['score'] ?>">
         </div>
         <div>
             <label for="impression">感想</label>
-            <textarea type="text" id="impression" name="impression" rows="10"></textarea>
+            <textarea type="text" id="impression" name="impression" rows="10"><?php echo $review['impression'] ?></textarea>
         </div>
         <button type="submit">登録する</button>
     </form>
